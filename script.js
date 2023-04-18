b52c485168090ad11a8c62b02da5f6e62d5838f9
Tahir Mughal <tahirmughal1198@gmail.com>
2023-04-18 19:58:28 +0500

    Code cleaned

--- script.js
+++ script.js
@@ -1,80 +1,7 @@
 
 const apiKey = 'd2d71685649644f4a35b27d7e43b22ad';
 const mainDiv = document.getElementById('mainDiv');
-// ingApi = `https://api.spoonacular.com/recipes/${element.id}/ingredientWidget.json?apiKey=${apiKey}`
-
 showAllData();
-
-// function showAllData() {
-//     let allapi = `https://api.spoonacular.com/recipes/complexSearch?number=15&apiKey=${apiKey}`
-
-
-
-//     // allData is variable/key that store all recipes data
-//     if (localStorage.getItem('allData') === null) {
-//         // console.log("if condition works");
-//         fetch(allapi)
-//             .then(response => response.json())
-//             .then(data => localStorage.setItem('allData', JSON.stringify(data)))
-//             .catch(error => console.error(error));
-//     }
-//     let data = JSON.parse(localStorage.getItem('allData'));
-
-//     // // to check the results in console
-//     // console.log(data);
-
-//     // data.results.forEach(element => {
-//     for (const element of data.results) {
-//         let foodCard = document.createElement('div');
-//         // creating class named as foodCard for styling
-//         foodCard.classList.add('foodCard');
-//         // foodCard.className = 'foodCard';
-//         // giving id for event listeners
-//         foodCard.id = element.id;
-
-//         foodCard.innerHTML = `<h2>Title : ${element.title}</h2>
-//                            <img src="${element.image}" alt="${element.title}" >
-//                            <h4>ID : ${element.id}</h4>`;
-//         mainDiv.appendChild(foodCard);
-//         // let gainid
-//         let ingApi = `https://api.spoonacular.com/recipes/${element.id}/ingredientWidget.json?apiKey=${apiKey}`;
-
-//         if (localStorage.getItem(search + element.id) === null) {
-//             await fetch(ingApi)
-//                 .then(response => response.json())
-//                 .then(data => localStorage.setItem(search + element.id, JSON.stringify(data)))
-//                 .catch(error => console.error(error));
-//         }
-
-//         let ingData = JSON.parse(localStorage.getItem(search + element.id));
-
-//         let ingredientsText = '';
-//         ingData.ingredients.forEach(element => {
-//             ingredientsText += element.name + ' ,';
-//         });
-
-//         let title = document.createElement('h2');
-//         title.innerHTML = "Ingredients";
-//         foodCard.appendChild(title);
-
-//         let ingredientsPara = document.createElement('p');
-//         ingredientsPara.className = 'ing';
-//         ingredientsPara.innerHTML = ingredientsText;
-//         foodCard.appendChild(ingredientsPara);
-
-//     };
-
-
-//     // // kuch fazool kaam
-//     // console.log(data)
-//     // for (const key of data.re) {
-//     //     console.log(key);
-//     // }
-//     // newdiv.innerHTML = localStorage.getItem('searchData');
-
-// }
-
-
 async function showAllData() {
     let allapi = `https://api.spoonacular.com/recipes/complexSearch?number=15&apiKey=${apiKey}`
 
@@ -134,78 +61,6 @@ async function showAllData() {
 
 
 
-
-// let searchBtn = document.getElementById('searchBtn');
-// searchBtn.addEventListener('click', searchfnc);
-// async function searchfnc() {
-//     mainDiv.innerHTML = "";
-//     var searchtxt = document.getElementById('searchInput').value;
-//     var search = searchtxt.toLowerCase();
-//     let searchApi = `https://api.spoonacular.com/recipes/complexSearch?number=15&query=${search}&apiKey=${apiKey}`
-
-//     if (localStorage.getItem(search) === null) {
-//         // console.log("if condition works");
-//         await fetch(searchApi)
-//             .then(response => response.json())
-//             .then(data => localStorage.setItem(search, JSON.stringify(data)))
-//             .catch(error => console.error(error));
-//         // newdiv.innerHTML = localStorage.getItem('searchData');
-//     }
-
-//     // console.log(search);
-//     let searchData = JSON.parse(localStorage.getItem(search));
-
-//     searchData.results.forEach(element => {
-//         let foodCard = document.createElement('div');
-//         foodCard.classList.add('foodCard');
-//         // foodCard.className = 'foodCard';
-//         foodCard.id = element.id;
-//         foodCard.innerHTML = `<h2>Title : ${element.title}</h2>
-//         <img src="${element.image}" alt="${element.title}" >
-//     <h4>ID : ${element.id}</h4>`
-//         mainDiv.appendChild(foodCard);
-
-
-
-//         let ingApi = `https://api.spoonacular.com/recipes/${element.id}/ingredientWidget.json?apiKey=${apiKey}`
-
-//         if (localStorage.getItem(search + `${element.id}`) === null) {
-//             // console.log("if condition works");
-//             fetch(ingApi)
-//                 .then(response => response.json())
-//                 .then(data => localStorage.setItem(search + `${element.id}`, JSON.stringify(data)))
-//                 .catch(error => console.error(error));
-//         }
-
-//         let ingData = JSON.parse(localStorage.getItem(search + `${element.id}`));
-
-//         // ingData.ingredients.forEach(element => {
-//         //     let Ingredients = document.createElement('p');
-//         //     Ingredients.className = 'ing';
-
-
-//         //     Ingredients.innerHTML = element.name;
-//         //     foodCard.appendChild(Ingredients);
-//         // })
-
-
-//         let ingredientsText = '';
-//         ingData.ingredients.forEach(element => {
-//             ingredientsText += element.name + ' ,';
-//         });
-
-//         let title = document.createElement('h2');
-//         title.innerHTML = "Ingredients";
-//         foodCard.appendChild(title);
-
-//         let ingredientsPara = document.createElement('p');
-//         ingredientsPara.className = 'ing';
-//         ingredientsPara.innerHTML = ingredientsText;
-//         foodCard.appendChild(ingredientsPara);
-
-//     });
-// }
-
 let searchBtn = document.getElementById('searchBtn');
 searchBtn.addEventListener('click', searchfnc);
 
@@ -261,145 +116,22 @@ async function searchfnc() {
         let button = document.createElement('button');
         button.innerHTML = "Click here";
         foodCard.appendChild(button);
+
+        button.addEventListener('click', recFunc)
+
     }
+    // document.getElementById('searchInput').innerHTML = '';
 }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // To check recipe
-
-// let checkrecipe = document.getElementsByClassName('foodCard');
-// // console.log(checkrecipe);
-// function showrecipe(id) {
-//     console.log(id);
-//     var recipe = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`
-
-//     // if (localStorage.getItem('recipes') === null) {
-//     //     fetch(recipe)
-//     //         .then(response => response.json())
-//     //         .then(data => localStorage.setItem("recipes", JSON.stringify(data)))
-//     //         .catch(error => console.error(error));
-
-//     // }
-// }
-// // checkrecipe is object that is being converted into array
-// const array = Array.from(checkrecipe);
-// array.forEach(function (elem) {
-//     // console.log(elem.id);
-//     elem.addEventListener("click", () => { showrecipe(elem.id) });
-
-// }
-// )
-
-
-// var recipedata = JSON.parse(localStorage.getItem('recipes'));
-
-// var allSteps = recipedata.analyzedInstructions[0].steps;
-// // console.log(steps);
-// allSteps.forEach(function (elem) {
-//     // console.log(elem);
-//     console.log(elem.step);
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     {
-// var recipe = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`
-//     fetch(recipe)
-//             .then(response => response.json())
-//             .then(data => console.log(data))
-//             .catch(error => console.error(error));
-//     });
-// });
-// // checkrecipe.addEventListener('click', showrecipe);
-// checkrecipe.from(elements).forEach(function(element) {
-//     element.addEventListener('click', recipe);
-//     recipe.myParam =
-//   });
-
-
-
-// // For Ingredients
-
-// let getid = JSON.parse(localStorage.getItem(636756))
-// if (getid) {
-//     console.log(getid)
-// }
-// else {
-
-
-
-// let data = JSON.parse(localStorage.getItem(636756))
-// console.log(data)
-// }
-
-
-
-
-
-
-// var recipe = `https://api.spoonacular.com/recipes/715495/information?apiKey=${apiKey}`
-// fetch(recipe)
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-//     .catch(error => console.error(error))
-
-
-
-
-// const recDiv = document.getElementById('recDiv');
-
-// async function recFunc() {
-//     let recipe = `https://api.spoonacular.com/recipes/715495/information?apiKey=${apiKey}`;
-
-//     if (localStorage.getItem(715495 + ' rec') === null) {
-//         fetch(recipe)
-//             .then(response => response.json())
-//             .then(data => localStorage.setItem(715495 + ' rec', JSON.stringify(data)))
-//             .catch(error => console.error(error));
-//     }
-
-//     let recData = JSON.parse(localStorage.getItem(715495 + ' rec'));
-//     // console.log(recData)
-
-//     let recCard = document.createElement('div');
-//     recCard.classList.add('recCard');
-//     recCard.id = recData.id;
-//     recCard.innerHTML = `<h2>inst: ${recData.instructions}</h2>
-//                            <h2>summ: ${recData.summary}</h2>`;
-//     recDiv.appendChild(recCard);
-// }
-
 const recDiv = document.getElementById('recDiv');
 
 async function recFunc() {
     mainDiv.style.display = 'none';
+    // document.getElementById('nav').innerHTML = "";
 
     let abc = this.previousElementSibling
     let x = abc.previousElementSibling
@@ -433,7 +165,9 @@ async function recFunc() {
 }
 function closeFun() {
     mainDiv.style.display = 'flex';
+    // document.getElementById('nav').style.display = "flex";
     // recDiv.style.display = 'none';
-    document.getElementById("recDiv").innerHTML = ""
+    recDiv.innerHTML = ""
+
 
 }
